d99b114064937d3761d1c5fc0adb7c8197093db7
Ars3n3-Lupin <ars3n3_lupin@outlook.com>
2023-08-11 14:32:59 +0200

    Database length added

--- encryption/encryption.bat
+++ encryption/encryption.bat
@@ -3,12 +3,12 @@ setlocal EnableDelayedExpansion
 cls
 goto :main
 
-:length <inputVar> <inputLenVar>
+:lengthFunction <inputVar> <inputLenVar>
 (
-	set /a %2=0
-	for /l %%i in ( 0, 1, 8191) do (
-		if "!%1:~%%i,1!" neq "" (
-			set /a %2+=1
+	set /a %~2=0
+	for /l %%i in ( 0, 1, 8190) do (
+		if "!%~1:~%%i,1!" neq "" (
+			set /a %~2+=1
 		)
 	)
 	echo Len !%~2!
@@ -21,17 +21,17 @@ setlocal
 	set database=..........0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 	set /p input=Encrypt your password: 
 	
-	call :length input inputLenVar
-
-	for /l %%g in ( 10, 1, 94 ) do (
+	call :lengthFunction database databaseLen
+	set /a databaseLen-=1
+	call :lengthFunction input inputLenVar
+	
+	for /l %%g in ( 10, 1, !databaseLen! ) do (
 		for /f %%h in ( "!database:~%%g,1!" ) do (
 			REM set "input=!input:%%h=-%%g!"
 			
 			for /l %%i in ( 0, 1, !inputLenVar! ) do (
-				if "!input:~%%i,1!" neq "" (
-					if "!input:~%%i,1!" == "!database:~%%g,1!"  (
+				if "!input:~%%i,1!" == "!database:~%%g,1!"  (
 					set "input=!input:%%h=%%g!"
-					)
 				)
 			)
 		)
